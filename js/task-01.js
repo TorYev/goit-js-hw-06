22f4d620df63586a860acf9a69b8df3998d62a9b
Yevhen Tordiia <138933442+TorYev@users.noreply.github.com>
2023-10-25 21:19:32 +0200

    Delete js directory

--- js/task-01.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-console.log(
-  `Number of categories: ${document.querySelectorAll("li.item").length}`
-);
-
-document
-  .querySelectorAll("li.item")
-  .forEach((key, index) => console.log(key.textContent));
