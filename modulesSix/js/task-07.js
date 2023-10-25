4f8a077320626e27cb7c03856a9458de0ca1bc55
Yevhen Tordiia <138933442+TorYev@users.noreply.github.com>
2023-10-25 21:17:35 +0200

    Delete modulesSix directory

--- modulesSix/js/task-07.js
+++
@@ -1,6 +0,0 @@
-const fontSizeControl = document.getElementById("font-size-control");
-const span = document.getElementById("text");
-
-fontSizeControl.addEventListener("input", function () {
-  span.style.fontSize = `${fontSizeControl.value}px`;
-});
